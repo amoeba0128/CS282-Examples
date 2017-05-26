2db8a43773d22994fdd594fea1a370879dea35b5
amoeba0128 <yichun.peng@gmail.com>
2017-05-26 15:43:56 +0800

    新增範例第一講的範例2

---
+++ Lecture01-Examples/Examples02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入姓名:");
+            Console.WriteLine("你的姓名:{0}",
+                Console.ReadLine());
+        }
+
+    }
+
+}
